915e62b8f819b291f5158ecaf658f21b2a60b8e9
NeKhoiCoder <156047526+NeKhoiCoder@users.noreply.github.com>
2024-01-12 14:58:38 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+alert('Xin chào bạn cảm ơn vì đã tham gia');
+var username;
+username=prompt('Nhập tên của bạn vào đây :');
+confirm('Bạn là ' + username + ' à!');
+document.write('Welcome to my site ! ' + username);
